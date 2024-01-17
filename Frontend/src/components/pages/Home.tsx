9614a5680e580594e04954fd99c84d100f35b41d
Talita Iza Nurazmi <talitazmiza@gmail.com>
2024-01-18 00:02:41 +0700

    feat!: remove/change unnecessary components

--- Frontend/src/components/pages/Home.tsx
+++ Frontend/src/components/pages/Home.tsx
@@ -1,22 +1,23 @@
 import BookingSteps from "../organs/BookingSteps"
 import HeroSection from "../organs/HeroSection"
-import NewsLetter from "../organs/NewsLetter"
-import Partners from "../organs/Partners"
+// import NewsLetter from "../organs/NewsLetter"
+// import Partners from "../organs/Partners"
 import Services from "../organs/Services"
 import Testimonials from "../organs/Testimonials"
 import TopDestination from "../organs/TopDestination"
-
+import NavBar from "../../components/organs/NavBar"
+import Footer from "../../components/organs/Footer"
 
 const Home = () => {
     return (
         <>
+            <NavBar />
             <HeroSection />
+            <Testimonials />
             <Services />
             <TopDestination />
             <BookingSteps />
-            <Testimonials />
-            <Partners />
-            <NewsLetter />
+            <Footer />
         </>
     )
 }
