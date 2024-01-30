e45682ad918fd14a0993112734f8e8b4f013f9f3
Talita Iza Nurazmi <talitazmiza@gmail.com>
2024-01-31 05:19:52 +0700

    feature: add navbar

---
+++ Frontend/src/components/pages/Admin/organs/Navbar.tsx
@@ -0,0 +1,22 @@
+const NavBar = () => {
+
+    return (
+        <div>
+            {/* This example requires Tailwind CSS v2.0+ */}
+            <div className="relative drop-shadow-md">
+                <div className="px-4 sm:px-6">
+                    <div className="flex justify-start items-center py-6 md:justify-start md:space-x-10">
+                        <div className="ml-10">
+                            <a href="/">
+                                <span className="text-orange-400">Bus Tourism</span>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
