b3071b06a8d217cf14d294268aa5fc4a246d6a90
Talita Iza Nurazmi <talitazmiza@gmail.com>
2024-01-31 05:15:58 +0700

    refactor: sidebar group menus

--- Frontend/src/components/pages/Admin/organs/Sidebar.tsx
+++ Frontend/src/components/pages/Admin/organs/Sidebar.tsx
@@ -1,73 +1,87 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faClock, faBus, faDoorOpen, faCity, faMoneyBill} from '@fortawesome/free-solid-svg-icons';
-import logo from "../../../../assets/logo.png"
-import {Image} from "../../../atoms/Image.tsx";
+import {faClock, faBus, faCity, faMoneyBill, faUser, faUsers, faUserLock} from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
     return (
-        <div className="bg-black">
-            <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar"
-                    type="button"
-                    className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-                <span className="sr-only">Open sidebar</span>
-                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <path clipRule="evenodd" fillRule="evenodd"
-                          d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-                </svg>
-            </button>
-
+        <div className="bg-gray-100">
             <aside id="logo-sidebar"
-                   className="fixed top-0 left-0 z-40 w-48 h-screen transition-transform -translate-x-full sm:translate-x-0"
+                   className="top-0 left-0 z-40 h-screen transition-transform -translate-x-full sm:translate-x-0"
                    aria-label="Sidebar">
-                <div className="h-full px-3 py-4 overflow-y-auto bg-white shadow-2xl">
-                    <a href="/" className="flex items-center pl-2.5 mb-5">
-                        <Image className="w-44" image={logo} alt="Logo" as="a" href="/" />
-                    </a>
-                    <ul className="space-y-2 font-medium">
-                        {/*<li>*/}
-                        {/*    <a href="/dashboard"*/}
-                        {/*       className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-orange-400 group">*/}
-                        {/*        <FontAwesomeIcon icon={faHouse} />*/}
-                        {/*        <span className="flex-1 ml-3 whitespace-nowrap">Dashboard</span>*/}
-                        {/*    </a>*/}
-                        {/*</li>*/}
-                        <li>
-                            <a href="/dashboard-fleet"
-                               className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-orange-400  group">
-                                <FontAwesomeIcon icon={faBus} />
-                                <span className="flex-1 ml-3 whitespace-nowrap">Armada</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#"
-                               className="flex items-center p-2 text-gray-300 rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-orange-400  group">
-                                <FontAwesomeIcon icon={faClock} />
-                                <span className="flex-1 ml-3 whitespace-nowrap">Jadwal</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/dashboard"
-                               className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-orange-400  group">
-                                <FontAwesomeIcon icon={faCity} />
-                                <span className="flex-1 ml-3 whitespace-nowrap">Destinasi</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/dashboard-transaction"
-                               className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-orange-400  group">
-                                <FontAwesomeIcon icon={faMoneyBill} />
-                                <span className="flex-1 ml-3 whitespace-nowrap">Transaksi</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/"
-                               className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-red-400 group">
-                                <FontAwesomeIcon icon={faDoorOpen} />
-                                <span className="flex-1 ml-3 whitespace-nowrap">Logout</span>
-                            </a>
-                        </li>
-                    </ul>
+                <div className="h-full px-3 py-4 overflow-y-auto">
+                    <div className="h-full p-10 rounded-2xl bg-white drop-shadow-md">
+                        <ul className="grid gap-2 space-y-2 font-medium">
+                            <li>
+                                <a href="/dashboard-fleet"
+                                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-orange-400  group">
+                                    <FontAwesomeIcon icon={faBus} />
+                                    <span className="flex-1 ml-3 whitespace-nowrap">Armada</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/dashboard-schedule"
+                                   className="flex items-center p-2 rounded-lg  hover:bg-gray-100 dark:hover:bg-orange-400  group">
+                                    <FontAwesomeIcon icon={faClock} />
+                                    <span className="flex-1 ml-3 whitespace-nowrap">Jadwal</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/dashboard-destination"
+                                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-orange-400  group">
+                                    <FontAwesomeIcon icon={faCity} />
+                                    <span className="flex-1 ml-3 whitespace-nowrap">Destinasi</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/dashboard-transaction"
+                                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-orange-400  group">
+                                    <FontAwesomeIcon icon={faMoneyBill} />
+                                    <span className="flex-1 ml-3 whitespace-nowrap">Transaksi</span>
+                                </a>
+                            </li>
+                            <h1 className="text-gray-400">MANAJEMEN USER</h1>
+                            <li>
+                                <a href="/dashboard-user"
+                                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-orange-400  group">
+                                    <FontAwesomeIcon icon={faUser} />
+                                    <span className="flex-1 ml-3 whitespace-nowrap">User</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-orange-400  group">
+                                    <FontAwesomeIcon icon={faUsers} />
+                                    <span className="flex-1 ml-3 whitespace-nowrap">Role</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/setting-permission"
+                                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-orange-400  group">
+                                    <FontAwesomeIcon icon={faUserLock} />
+                                    <span className="flex-1 ml-3 whitespace-nowrap">Role Permission</span>
+                                </a>
+                            </li>
+                            <h1 className="text-gray-400">AUTH</h1>
+                            <li>
+                                <a href="#"
+                                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-orange-400  group">
+                                    <span className="flex-1 ml-3 whitespace-nowrap">Sign In</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-orange-400  group">
+                                    <span className="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/"
+                                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-red-400 group">
+                                    {/*<FontAwesomeIcon icon={faDoorOpen} />*/}
+                                    <span className="flex-1 ml-3 whitespace-nowrap">Logout</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </aside>
         </div>
