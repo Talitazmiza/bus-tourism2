7e3f222f0ccfc99af0ac1caed3da2f3471bac74f
Talita Iza Nurazmi <talitazmiza@gmail.com>
2024-02-06 14:27:55 +0700

    fix: remove TODO

--- Frontend/src/components/pages/Admin/components/DestinationTable.tsx
+++ Frontend/src/components/pages/Admin/components/DestinationTable.tsx
@@ -36,8 +36,6 @@ export default function BasicTable() {
     return (
         <div className="p-20">
             <div className="flex justify-between w-full mb-5">
-                {/*TODO*/}
-                {/*SearchBar*/}
                 <SearchBar />
                 <AddDestPopUp />
             </div>
@@ -86,8 +84,7 @@ export default function BasicTable() {
                                                         <svg className="w-3 h-3" aria-hidden="true"
                                                              xmlns="http://www.w3.org/2000/svg" fill="none"
                                                              viewBox="0 0 14 14">
-                                                            <path stroke="currentColor" stroke-linecap="round"
-                                                                  stroke-linejoin="round" stroke-width="2"
+                                                            <path stroke="currentColor"
                                                                   d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                                                         </svg>
                                                         <span className="sr-only">Close modal</span>
@@ -97,8 +94,7 @@ export default function BasicTable() {
                                                             className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
                                                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                                             fill="none" viewBox="0 0 20 20">
-                                                            <path stroke="currentColor" stroke-linecap="round"
-                                                                  stroke-linejoin="round" stroke-width="2"
+                                                            <path stroke="currentColor"
                                                                   d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                                                         </svg>
                                                         <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Apakah anda yakin ingin menghapus Destinasi ini?</h3>
